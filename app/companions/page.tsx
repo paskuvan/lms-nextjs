a9fc00a1fe590997dbfe535bfd42d92736a2c70d
paskuvan <majopaskuvan@gmail.com>
2025-06-28 10:02:33 -0400

    update

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
-const page = () => {
+const CompanionsLibrary = () => {
   return (
-    <div>Companions</div>
+    <div>Companions Library</div>
   )
 }
 
-export default page
+export default CompanionsLibrary
