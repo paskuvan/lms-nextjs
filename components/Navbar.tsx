a92057b2a37c0c6dd0e5894b9c2ba60aa47e932c
paskuvan <majopaskuvan@gmail.com>
2025-06-30 12:31:37 -0400

    up

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,23 @@
 const Navbar = () => {
   return (
-    <nav>Navbar</nav>
+    <nav className="navbar">
+    <link href="/">
+    <div className="flex items-center gap-2.5 cursor-pointer">
+        <Image 
+        src="/images/logo.svg" 
+        alt="Logo" 
+        width={46} 
+        height={44} 
+        />
+    </div>
+    <div className="flex items-center gap-8">
+        <p>Home</p>
+        <p>Companions</p>
+        <p>My Journey</p>
+        <p>Sign In</p>
+    </div>
+    </link>    
+    </nav>
   )
 }
 
