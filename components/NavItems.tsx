462751dff1d14e645db77d6221ae6023b809025d
paskuvan <majopaskuvan@gmail.com>
2025-07-14 15:43:00 -0400

    navbar items

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
+
+const navItems = [
+    { label:'Home', href: '/' },
+    { label: 'Companions', href: '/companions' },
+    { label: 'My Journey', href: 'my-journey' },
+]
+
 const NavItems = () => {
   return (
-    <div>NavItems</div>
+    <nav className='flex items-center gap-4'>
+     Nav items
+      </nav>
   )
 }
 
